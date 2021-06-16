08781eb9d31ae301f3352e65a102c6cbcd075b49
Zayar Tun <kyawzayartun.dev@gmail.com>
2021-06-16 17:12:32 +0630

    connect to database - mongodb

--- backend/server.js
+++ backend/server.js
@@ -1,9 +1,11 @@
 import express from 'express'
 import dotenv from 'dotenv'
 
+import connectDb from './config/db.js'
 import products from './data/products.js'
 
 dotenv.config()
+connectDb()
 
 const app = express()
 
