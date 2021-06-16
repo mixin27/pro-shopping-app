c5fb9495120a03cfe19fb3b621b489068cb3161b
Zayar Tun <kyawzayartun.dev@gmail.com>
2021-06-17 01:38:58 +0630

    implement custom authentication middleware

---
+++ backend/middlewares/auth.js
@@ -0,0 +1,32 @@
+import asyncHandler from 'express-async-handler'
+import jwt from 'jsonwebtoken'
+
+import User from '../models/userModel.js'
+
+const protect = asyncHandler(async (req, res, next) => {
+  let token
+
+  if (
+    req.headers.authorization &&
+    req.headers.authorization.startsWith('Bearer')
+  ) {
+    try {
+      token = req.headers.authorization.split(' ')[1]
+
+      const decoded = jwt.verify(token, process.env.JWT_SECRET)
+      req.user = await User.findById(decoded.id).select('-password')
+    } catch (error) {
+      res.status(401)
+      throw new Error('Not authorized. Token validation failed.')
+    }
+  }
+
+  if (!token) {
+    res.status(401)
+    throw new Error('Not authorized. No token provided.')
+  }
+
+  next()
+})
+
+export { protect }
--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -3,10 +3,12 @@
  */
 import express from 'express'
 
-import { authUser } from '../controllers/userController.js'
+import { protect } from '../middlewares/auth.js'
+import { authUser, getUserProfile } from '../controllers/userController.js'
 
 const router = express.Router()
 
 router.post('/login', authUser)
+router.route('/profile').get(protect, getUserProfile)
 
 export default router
