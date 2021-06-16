f3e31b7ae066d68ffc23642a46056a1a1ee794e8
Zayar Tun <kyawzayartun.dev@gmail.com>
2021-06-17 00:41:36 +0630

    implement auth endpoint and login route

--- backend/server.js
+++ backend/server.js
@@ -5,17 +5,21 @@ import colors from 'colors'
 import { notfound, errorHandler } from './middlewares/errors.js'
 import connectDb from './config/db.js'
 import productRoutes from './routes/productRoutes.js'
+import userRoutes from './routes/userRoutes.js'
 
 dotenv.config()
 connectDb()
 
 const app = express()
 
+app.use(express.json())
+
 app.get('/', (req, res) => {
   res.send('API is running...')
 })
 
 app.use('/api/products', productRoutes)
+app.use('/api/users', userRoutes)
 
 app.use(notfound)
 app.use(errorHandler)
