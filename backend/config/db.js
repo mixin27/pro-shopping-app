8c593dc8761642ddff9e2d20e5e5f76ed3ea36bb
Zayar Tun <kyawzayartun.dev@gmail.com>
2021-06-16 17:18:46 +0630

    adding colors to console

--- backend/config/db.js
+++ backend/config/db.js
@@ -7,9 +7,9 @@ const connectDb = async () => {
       useNewUrlParser: true,
       useCreateIndex: true,
     })
-    console.log(`MongoDB Connected: ${conn.connection.host}`)
+    console.log(`MongoDB Connected: ${conn.connection.host}`.cyan.underline)
   } catch (error) {
-    console.error(`Error: ${error.message}`)
+    console.error(`Error: ${error.message}`.red.underline.bold)
     process.exit(1)
   }
 }
