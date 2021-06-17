683c7407df94c05f8c14ec0c13bcf89a85f25dae
Zayar Tun <kyawzayartun.dev@gmail.com>
2021-06-17 18:55:30 +0630

    implement payment screen and save payment method

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -11,6 +11,7 @@ import LoginScreen from './screens/LoginScreen'
 import RegisterScreen from './screens/RegisterScreen'
 import ProfileScreen from './screens/ProfileScreen'
 import ShippingScreen from './screens/ShippingScreen'
+import PaymentScreen from './screens/PaymentScreen'
 
 const App = () => {
   return (
@@ -18,6 +19,7 @@ const App = () => {
       <Header />
       <main className='py-3'>
         <Container>
+          <Route path='/payment' component={PaymentScreen} />
           <Route path='/shipping' component={ShippingScreen} />
           <Route path='/profile' component={ProfileScreen} />
           <Route path='/register' component={RegisterScreen} />
