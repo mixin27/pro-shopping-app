298ab18a23b151e1b23fd6c008fe8e7d7f845206
Zayar Tun <kyawzayartun.dev@gmail.com>
2021-06-17 18:07:53 +0630

    implement shipping screen and saving address

--- frontend/src/store.js
+++ frontend/src/store.js
@@ -32,9 +32,14 @@ const userInfoFromStorage = localStorage.getItem('userInfo')
   ? JSON.parse(localStorage.getItem('userInfo'))
   : null
 
+const shippingAddressFromStorage = localStorage.getItem('shippingAddress')
+  ? JSON.parse(localStorage.getItem('shippingAddress'))
+  : {}
+
 const initialState = {
   cart: {
     cartItems: cartItemsFromStorage,
+    shippingAddress: shippingAddressFromStorage,
   },
   userLogin: {
     userInfo: userInfoFromStorage,
