dea5b1ef81f2b74d9b90c8994203d235a7566845
Zayar Tun <kyawzayartun.dev@gmail.com>
2021-06-18 11:39:02 +0630

    admin user update

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -156,7 +156,7 @@ const updateUser = asyncHandler(async (req, res) => {
 
   user.name = req.body.name || user.name
   user.email = req.body.email || user.email
-  user.isAdmin = req.body.isAdmin || user.isAdmin
+  user.isAdmin = req.body.isAdmin
 
   const updatedUser = await user.save()
   res.json({
