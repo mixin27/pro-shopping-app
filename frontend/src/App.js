d0b56c3f0eb02c2ff55609b51bd8b434b3222a5f
Zayar Tun <kyawzayartun.dev@gmail.com>
2021-06-18 12:17:54 +0630

    implement admin product list

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -16,6 +16,7 @@ import PlaceOrderScreen from './screens/PlaceOrderScreen'
 import OrderScreen from './screens/OrderScreen'
 import UserListScreen from './screens/UserListScreen'
 import UserEditScreen from './screens/UserEditScreen'
+import ProductListScreen from './screens/ProductListScreen'
 
 const App = () => {
   return (
@@ -23,6 +24,7 @@ const App = () => {
       <Header />
       <main className='py-3'>
         <Container>
+          <Route path='/admin/productlist' component={ProductListScreen} />
           <Route path='/admin/user/:id/edit' component={UserEditScreen} />
           <Route path='/admin/userlist' component={UserListScreen} />
           <Route path='/order/:id' component={OrderScreen} />
