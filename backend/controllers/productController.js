de602c5d25925a447874b1ae0104114f95db759f
Zayar Tun <kyawzayartun.dev@gmail.com>
2021-06-20 00:02:39 +0630

    change sample picture

--- backend/controllers/productController.js
+++ backend/controllers/productController.js
@@ -67,7 +67,7 @@ const createProduct = asyncHandler(async (req, res) => {
     name: 'Sample name',
     price: 0,
     user: req.user._id,
-    image: '/images/sample.jpg',
+    image: '/images/sample.png',
     brand: 'sample brand',
     category: 'sample category',
     countInStock: 0,
